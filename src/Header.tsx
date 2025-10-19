5df7dc1b0a109cbdf58fedfa4b23f9b1f6ec8f95
prasad <nagunuri.prasad5@gmail.com>
2025-10-19 15:40:42 +0530

    changes

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,22 @@
-// src/Header.tsx
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import companyLogo from './assets/bookmyparty.png';
-import './assets/css/Header.css';
-import LanguageSelector from './LanguageSelector';
-import { FaMapMarkerAlt } from 'react-icons/fa';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import companyLogo from "./assets/bookmyparty.png";
+import "./assets/css/Header.css";
+import LanguageSelector from "./LanguageSelector";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Header = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState('Login/Signup');
-  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState("Login/Signup");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showLocationPopup, setShowLocationPopup] = useState(false);
+
+  // Manual location fields
+  const [manualLocation, setManualLocation] = useState({
+    state: "",
+    city: "",
+    pincode: "",
+  });
 
   const handleLanguageChange = (language: string) => {
     setSelectedLanguage(`Login/Signup (${language})`);
@@ -17,20 +24,24 @@ const Header = () => {
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
-    console.log('Searching for:', e.target.value);
+    console.log("Searching for:", e.target.value);
+  };
+
+  const handleLocationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setManualLocation((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
     <header className="header">
       <div className="header-container">
-        {/* Left: Logo, Location, Search */}
+        {/* Left Section */}
         <div className="d-flex align-items-center gap-3">
-          <NavLink  to="/">
+          <NavLink to="/">
             <img src={companyLogo} alt="CompanyLogo" className="logo-img" />
           </NavLink>
 
-
-          {/* Search Input */}
+          {/* Search Box */}
           <div className="search-container">
             <input
               type="text"
@@ -40,19 +51,48 @@ const Header = () => {
               onChange={handleSearchChange}
             />
           </div>
-          
-          {/* Location Icon */}
-          <a
-            href="https://www.google.com/maps/@17.4334334,78.4418009,17z?entry=ttu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="location-link-icon"
+
+          {/* Location Icon + Popup */}
+          <div
+            className="location-wrapper"
+            onMouseEnter={() => setShowLocationPopup(true)}
+            onMouseLeave={() => setShowLocationPopup(false)}
           >
-            <FaMapMarkerAlt size={22} />
-          </a>
+            <FaMapMarkerAlt size={22} className="location-link-icon" />
+
+            {showLocationPopup && (
+              <div className="location-popup">
+                <p className="popup-title">Enter Location</p>
+                <input
+                  type="text"
+                  name="state"
+                  placeholder="State"
+                  value={manualLocation.state}
+                  onChange={handleLocationChange}
+                  className="location-input"
+                />
+                <input
+                  type="text"
+                  name="city"
+                  placeholder="City"
+                  value={manualLocation.city}
+                  onChange={handleLocationChange}
+                  className="location-input"
+                />
+                <input
+                  type="text"
+                  name="pincode"
+                  placeholder="Pincode"
+                  value={manualLocation.pincode}
+                  onChange={handleLocationChange}
+                  className="location-input"
+                />
+              </div>
+            )}
+          </div>
         </div>
 
-        {/* Right: Login + Language */}
+        {/* Right Section */}
         <div className="login-container d-flex align-items-center gap-3">
           <NavLink className="login-button" to="/LoginForm">
             {selectedLanguage}
